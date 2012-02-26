a4329f8399466bd92f8e7fa0ccbdd6b5315f5303
Andrew Carter <andrew.r.carter@gmail.com>
2012-02-26 03:37:41 -0500

    Initial Commit

---
+++ Spinner/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Spinner
+//
+//  Created by Andrew Carter on 2/26/12.
+//  Copyright (c) 2012 WillowTree Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
